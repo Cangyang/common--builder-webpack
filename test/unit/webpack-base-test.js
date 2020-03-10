f09d9c694c8572131ce86d59dd1686de14a00557
Cangyang <ytf25206@gmail.com>
2020-03-11 00:26:42 +0800

    feat: fix the unit test result on travis

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -6,7 +6,11 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
 
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, 'D:/Study/webpack/common-builder-webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, 'D:/Study/webpack/common-builder-webpack/test/smoke/template/src/search/index.js');
+		// for local
+        // assert.equal(baseConfig.entry.index, 'D:/Study/webpack/common-builder-webpack/test/smoke/template/src/index/index.js');
+        // assert.equal(baseConfig.entry.search, 'D:/Study/webpack/common-builder-webpack/test/smoke/template/src/search/index.js');
+		// for travis
+        assert.equal(baseConfig.entry.search, '/home/travis/build/Cangyang/common-builder-webpack/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index, '/home/travis/build/Cangyang/common-builder-webpack/test/smoke/template/src/index/index.js');
     });
 });
