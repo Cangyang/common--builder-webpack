57413431342e5f2f4b104bb2cd94298c08eafa57
Cangyang <ytf25206@gmail.com>
2020-03-11 00:41:04 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -6,11 +6,8 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
 
     it('entry', () => {
-		// for local
-        // assert.equal(baseConfig.entry.index, 'D:/Study/webpack/common-builder-webpack/test/smoke/template/src/index/index.js');
-        // assert.equal(baseConfig.entry.search, 'D:/Study/webpack/common-builder-webpack/test/smoke/template/src/search/index.js');
 		// for travis
-        assert.equal(baseConfig.entry.search, '/home/travis/build/Cangyang/common-builder-webpack/test/smoke/template/src/search/index.js');
-        assert.equal(baseConfig.entry.index, '/home/travis/build/Cangyang/common-builder-webpack/test/smoke/template/src/index/index.js');
+        assert.equal(baseConfig.entry.search.indexOf('common-builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
+        assert.equal(baseConfig.entry.index.indexOf('common-builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
     });
 });
